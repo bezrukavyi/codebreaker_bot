14ce6a8feb81a7761ff6fa292c9fc4c3d2cb7e79
bezrukavyi <yaroslav555@gmail.com>
2016-11-09 22:23:29 +0200

    Ended game

--- bot_game.rb
+++ bot_game.rb
@@ -1,8 +1,24 @@
+require 'yaml'
+
 class BotGame
 
   attr_accessor :game
 
+  DATA_PATH = File.join(File.dirname(__FILE__), 'scores.yml')
+  RULES = [
+    '------------Rules!-----------',
+    "You need guess secret code. This FOUR-digit number(guess) with symbols from 1 to 6",
+    "You have #{Codeguessing::Game::MAX_ATTEMPTS} attempt(s) and #{Codeguessing::Game::MAX_HINT} hint(s)",
+    "A '+' indicates an exact match: one of the numbers in the guess is the same as one of the numbers in the secret code and in the same position.",
+    "A '-' indicates a number match: one of the numbers in the guess is the same as one of the numbers in the secret code but in a different position.",
+    "A empty answer means that you nothing guessed",
+    "If you want get hint write 'hint'",
+    "If you want start game write your guess-number",
+    '-----------------------------'
+  ]
+
   def initialize
+    @scores = load(DATA_PATH) || []
     @game = Codeguessing::Game.new
   end
 
@@ -10,16 +26,48 @@ class BotGame
     guess = "Your guess: #{@game.guess(code)}\n"
     case @game.win?
     when true
-      "You win!"
+      "You win!\nYou can save your result with command save"
     when false
-      "You loose"
+      "You loose\nSecret code was #{@game.secret_code}"
     else
-      cur_score + guess
+      state + guess
     end
   end
 
-  def cur_score
+  def state
     message = "Attempts: #{@game.attempts} Hint: #{@game.hint_count}\n"
   end
 
+  def load(path)
+    YAML.load(File.open(path)) if File.exist?(path)
+  end
+
+  def save!(name = 'Anonim')
+    return 'You cant save game' unless @game.win?
+    score = @game.cur_score(name)
+    @scores << score
+    File.new(DATA_PATH, 'w') unless File.exist?(DATA_PATH)
+    File.open(DATA_PATH, "r+") { |f| f.write(@scores.to_yaml) }
+    parse_score(score)
+  end
+
+  def scores
+    return 'Table of scores is empty' if @scores.empty?
+    message = ''
+    @scores.each do |score|
+      message += parse_score(score)
+    end
+    message
+  end
+
+  def parse_score(score)
+    score_msg = [
+      '--------------------',
+      "#{score[:name]}",
+      '--------------------',
+      "Attempts: #{score[:attempts]}",
+      "Hint: #{score[:hint_count]}",
+      "Date: #{Time.at(score[:date]).strftime("%d %B %Y")}\n",
+    ].join("\n")
+  end
 end
--- main.rb
+++ main.rb
@@ -5,43 +5,61 @@ require "telegram/bot"
 token = '268384989:AAHxzzJGWQiVw8lDKGg0S8xntboUDVJ_S20'
 
 Telegram::Bot::Client.run(token) do |bot|
-  bot_breaker = BotGame.new
+  bot_game = BotGame.new
 
   bot.listen do |message|
     question = message.from.first_name
 
-    answers =
+    markup =
       Telegram::Bot::Types::ReplyKeyboardMarkup
-      .new(keyboard: [%w(hint attempts scores), %w(start exit replay)])
+      .new(keyboard: [%w(hint scores save), ['new game'], ['give up']])
 
     case message.text
     when /start/i
       bot.api.send_message(
         chat_id: message.chat.id,
-        text: "Let's breake all secret")
+        text: "#{message.from.first_name}, wellcome to Codeguessing!\nIf you want to know about the rules enter 'rules' ",
+        reply_markup: markup)
 
     when /^[1-6]{#{Codeguessing::Game::MAX_SIZE}}$/s
       bot.api.send_message(
         chat_id: message.chat.id,
-        text: bot_breaker.go(message.text))
+        text: bot_game.go(message.text))
 
-    when /replay/i
-      bot_breaker = TelegramGame.new
+    when /new game/i
+      bot_game = BotGame.new
       bot.api.send_message(
         chat_id: message.chat.id,
-        text: "New Game\n#{bot_breaker.cur_score}")
+        text: "#{bot_game.state}\nNew Game")
 
     when /hint/i
-      hint = bot_breaker.game.hint
-      hint_msg = bot_breaker.cur_score + "Hint: #{hint}\n"
+      hint = bot_game.game.hint
+      hint_msg = bot_game.state + "Hint: #{hint}\n"
       bot.api.send_message(
         chat_id: message.chat.id,
         text: hint_msg)
 
-    when /stop/i
+    when /rules/i
       bot.api.send_message(
         chat_id: message.chat.id,
-        text: "Bye, #{message.from.first_name}")
+        text: BotGame::RULES.join("\n"))
+
+    when /save/i
+      bot.api.send_message(
+        chat_id: message.chat.id,
+        text: "#{bot_game.save!(message.from.first_name)}")
+        bot_game = BotGame.new
+
+    when /scores/i
+      bot.api.send_message(
+      chat_id: message.chat.id,
+      text: bot_game.scores)
+
+    when /give up/i
+      bot_game = BotGame.new
+      bot.api.send_message(
+        chat_id: message.chat.id,
+        text: "Sorry, #{message.from.first_name}, but secret code was #{bot_game.game.secret_code}")
 
     else
       bot.api.send_message(
