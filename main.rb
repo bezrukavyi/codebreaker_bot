0bd410af50d278c615345a5cd27bc1c37cc29b66
bezrukavyi <yaroslav555@gmail.com>
2016-11-10 22:33:48 +0200

    Fix same bot in diff chats

--- main.rb
+++ main.rb
@@ -5,70 +5,73 @@ require "telegram/bot"
 token = '268384989:AAHxzzJGWQiVw8lDKGg0S8xntboUDVJ_S20'
 
 Telegram::Bot::Client.run(token) do |bot|
-  bot_game = Botguessing.new
-
+  bots = {}
   bot.listen do |message|
-    question = message.from.first_name
+    message_id = message.chat.id
+    bots[message_id] ||= Botguessing.new
 
+    question = message.from.first_name
     markup =
       Telegram::Bot::Types::ReplyKeyboardMarkup
       .new(keyboard: [%w(hint scores save rules), ['new game'], ['give up']])
 
+
     case message.text
     when /start/i
+      bots[message_id] = Botguessing.new
       wellcome = [
         "#{message.from.first_name}, wellcome to Codeguessing!",
         "So, if you ready guess, click on keyboard 'new game'",
         "If you want to know about the rules click to 'rules'"
       ]
       bot.api.send_message(
-        chat_id: message.chat.id,
+        chat_id: message_id,
         text: wellcome.join("\n"),
         reply_markup: markup)
 
     when /^[1-6]{#{Codeguessing::Game::MAX_SIZE}}$/s
       bot.api.send_message(
-        chat_id: message.chat.id,
-        text: bot_game.go(message.text))
+        chat_id: message_id,
+        text: bots[message_id].go(message.text))
 
     when /new game/i
-      bot_game = Botguessing.new
+      bots[message_id] = Botguessing.new
       bot.api.send_message(
-        chat_id: message.chat.id,
-        text: "Bot maked secret code.\nLet's guess!\n#{bot_game.state}")
+        chat_id: message_id,
+        text: "Bot maked secret code.\nLet's guess!\n#{bots[message_id].state}")
 
     when /hint/i
-      hint = bot_game.game.hint
-      hint_msg = bot_game.state + "Hint: #{hint}\n"
+      hint = bots[message_id].game.hint
+      hint_msg = bots[message_id].state + "Hint: #{hint}\n"
       bot.api.send_message(
-        chat_id: message.chat.id,
+        chat_id: message_id,
         text: hint_msg)
 
     when /rules/i
       bot.api.send_message(
-        chat_id: message.chat.id,
+        chat_id: message_id,
         text: Botguessing::RULES.join("\n"))
 
     when /save/i
       bot.api.send_message(
-        chat_id: message.chat.id,
-        text: "#{bot_game.save!(message.from.first_name)}")
-        bot_game = Botguessing.new
+        chat_id: message_id,
+        text: "#{bots[message_id].save!(message.from.first_name)}")
+        bots[message_id] = Botguessing.new
 
     when /scores/i
       bot.api.send_message(
-      chat_id: message.chat.id,
-      text: bot_game.scores)
+      chat_id: message_id,
+      text: bots[message_id].scores)
 
     when /give up/i
-      bot_game = Botguessing.new
+      bots[message_id] = Botguessing.new
       bot.api.send_message(
-        chat_id: message.chat.id,
-        text: "Sorry, #{message.from.first_name}, but secret code was #{bot_game.game.secret_code}")
+        chat_id: message_id,
+        text: "Sorry, #{message.from.first_name}, but secret code was #{bots[message_id].game.secret_code}")
 
     else
       bot.api.send_message(
-        chat_id: message.chat.id,
+        chat_id: message_id,
         text: "Incorrect data #{message.text}. Read again rules")
     end
   end
